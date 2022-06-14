38b5854eb59de3695dbbe7d8a68864925fe7cf2c
Sheng Chuang Wong <shengchuang.wong@grabtaxi.com>
2022-06-15 00:15:46 +0800

    remove things 2

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,45 @@ let currentApp = app
 const server = http.createServer(app)
 
 server.listen(3000, () => {
-  console.log('web scraping server started on port 3000')
+  console.log('server started on port 3000')
+})
+
+if (module.hot) {
+  module.hot.accept('./server', () => {
+    server.removeListener('request', currentApp)
+    server.on('request', app)
+    currentApp = app
+  })
+}
+
+
+import http from 'http'
+import app from './server'
+
+let currentApp = app
+const server = http.createServer(app)
+
+server.listen(3000, () => {
+  console.log('server started on port 3000')
+})
+
+if (module.hot) {
+  module.hot.accept('./server', () => {
+    server.removeListener('request', currentApp)
+    server.on('request', app)
+    currentApp = app
+  })
+}
+
+
+import http from 'http'
+import app from './server'
+
+let currentApp = app
+const server = http.createServer(app)
+
+server.listen(3000, () => {
+  console.log('server started on port 3000')
 })
 
 if (module.hot) {
